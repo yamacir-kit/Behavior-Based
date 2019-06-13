9ff17fedd11c52f675eaf257b09f4dbb1c0202ae
yamacir-kit <httperror@404-notfound.jp>
2019-06-14 08:42:05 +0900

    Add package.xml.cmake

---
+++ configure/package.xml.cmake
@@ -0,0 +1,20 @@
+<?xml version="1.0"?>
+
+<package format="2">
+  <name>behavior_based</name>
+  <version>${MAJOR}.${MINOR}.${SUBMINOR}</version>
+
+  <description>
+    Behavior-based robotics libraries for ROS.
+  </description>
+
+  <license>BSD</license>
+
+  <buildtool_depend>catkin</buildtool_depend>
+
+  <depend>geometry_msgs</depend>
+  <depend>roscpp</depend>
+  <depend>sensor_msgs</depend>
+  <depend>std_msgs</depend>
+</package>
+
