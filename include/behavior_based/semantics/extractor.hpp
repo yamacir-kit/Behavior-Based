68d7feb959aea90324b88b74f798db57498076d9
yamacir-kit <httperror@404-notfound.jp>
2019-06-14 16:35:09 +0900

    Add semantics extractor

---
+++ include/behavior_based/semantics/extractor.hpp
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_BEHAVIOR_BASED_SEMANTICS_EXTRACTOR_HPP
+#define INCLUDED_BEHAVIOR_BASED_SEMANTICS_EXTRACTOR_HPP
+
+#include <behavior_based/configure.hpp>
+
+namespace NAMESPACE { namespace semantics
+{
+  template <typename Semantics, typename... Ts>
+  auto& extract(Ts&&... xs) noexcept
+  {
+    static Semantics semantics {std::forward<Ts>(xs)...};
+    return semantics;
+  }
+}} // namespace NAMESPACE::semantics
+
+#endif // INCLUDED_BEHAVIOR_BASED_SEMANTICS_EXTRACTOR_HPP
+
