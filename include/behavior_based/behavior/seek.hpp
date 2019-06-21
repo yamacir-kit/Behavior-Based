5fd3b63676d325dbb877c965e3cd1ea94507a386
yamacir-kit <httperror@404-notfound.jp>
2019-06-21 14:45:14 +0900

    Update seek to use cast to extract semantics

--- include/behavior_based/behavior/seek.hpp
+++ include/behavior_based/behavior/seek.hpp
@@ -2,7 +2,7 @@
 #define INCLUDED_BEHAVIOR_BASED_BEHAVIOR_SEEK_HPP
 
 #include <behavior_based/configure.hpp>
-#include <behavior_based/semantics/extractor.hpp>
+// #include <behavior_based/semantics/extractor.hpp>
 #include <behavior_based/semantics/velocity.hpp>
 
 namespace NAMESPACE { namespace behavior
@@ -13,17 +13,24 @@ namespace NAMESPACE { namespace behavior
     constexpr seek() = default;
 
     template <typename Environment>
-    auto operator()(Environment&& environment) const
+    auto operator()(const Environment& environment) const
       -> typename semantics::velocity_traits<Velocity>::output_type
     {
       using namespace semantics;
 
       const auto current {
-        extract<Velocity>().from(environment)
+        // extract<Velocity>().from(environment)
+        static_cast<const Velocity&>(environment)()
       };
 
+      // TODO
+      // セマンティクスの関数呼び出し演算子をキャスト演算子に変更して、
+      // ２段キャストをセマンティックキャストとしてAPIを用意すること
+
       const auto desired {
-        extract<Target>().from(environment).normalized() * velocity_traits<Velocity>::max
+          // extract<Target>().from(environment).normalized()
+          static_cast<const Target&>(environment)().normalized()
+        * velocity_traits<Velocity>::linear_max
       };
 
       return desired - current; // steering
