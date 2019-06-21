1c499972baa13713c57c9cec43b33014e986cdea
yamacir-kit <httperror@404-notfound.jp>
2019-06-21 10:10:11 +0900

    Add target semantics

--- include/behavior_based/semantics/target.hpp
+++ include/behavior_based/semantics/target.hpp
@@ -1,12 +1,25 @@
 #ifndef INCLUDED_BEHAVIOR_BASED_SEMANTICS_TARGET_HPP
 #define INCLUDED_BEHAVIOR_BASED_SEMANTICS_TARGET_HPP
 
+#include <sensor_msgs/Joy.h>
+
 #include <behavior_based/configure.hpp>
+#include <behavior_based/semantics/define.hpp>
 
 namespace NAMESPACE { namespace semantics
 {
-  template <typename...>
-  struct target;
+  DEFINE_SEMANTICS_CATEGORY(target, output_type::Zero());
+
+  template <typename Target>
+  struct target_traits
+  {
+    using output_type = typename Target::output_type;
+  };
+
+  DEFINE_SEMANTICS_CATEGORY_SPECIALIZATION(target, sensor_msgs::Joy,
+  {
+    return output_type {message->axes[1], message->axes[2]};
+  });
 }} // namespace NAMESPACE::semantics
 
 #endif // INCLUDED_BEHAVIOR_BASED_SEMANTICS_TARGET_HPP
