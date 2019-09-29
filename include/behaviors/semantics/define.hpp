6b3b017b36e529ade77e752969ae52710be5e3b5
yamacir-kit <httperror@404-notfound.jp>
2019-09-30 01:17:07 +0900

    Fix bug of semantics definer macro

--- include/behaviors/semantics/define.hpp
+++ include/behaviors/semantics/define.hpp
@@ -7,66 +7,48 @@
 #include <behaviors/semantics/facade.hpp>
 
 #ifndef NDEBUG
-#define DEFINE_SEMANTICS_CATEGORY(CATEGORY, DEFAULT_VECTOR)                    \
-template <typename...>                                                         \
-struct CATEGORY;                                                               \
-template <>                                                                    \
-struct CATEGORY<expression::unit>                                              \
-  : public facade<CATEGORY, expression::unit>                                  \
-  , public expression::unit                                                    \
-{                                                                              \
-  template <typename... Ts>                                                    \
-  constexpr CATEGORY(Ts&&... xs)                                               \
-    : expression::unit {std::forward<Ts>(xs)...}                               \
-  {}                                                                           \
-                                                                               \
-  template <typename... Ts>                                                    \
-  decltype(auto) operator()(Ts&&...) const noexcept                            \
-  {                                                                            \
-    return DEFAULT_VECTOR;                                                     \
-  }                                                                            \
-}
+#define PRINT_SEMANTICS_CATEGORY_INFORMATION(CATEGORY)                         \
+std::cerr << ";\n"                                                             \
+          << "; semantics\t; category " << #CATEGORY << std::endl;             \
+std::cerr << ";\t\t; invoked default value!\n"                                 \
+          << ";\t\t; two situations are possible;\n"                           \
+          << ";\t\t;   (1) the message has not been accepted (a.k.a meessage == nullptr)\n" \
+          << ";\t\t;   (2) called explicitly (in many cases, meaning no action is required)\n" \
+          << std::flush;
 #else // NDEBUG
-#define DEFINE_SEMANTICS_CATEGORY(CATEGORY, DEFAULT_VECTOR)                    \
-template <typename...>                                                         \
-struct CATEGORY;                                                               \
-template <>                                                                    \
-struct CATEGORY<expression::unit>                                              \
-  : public facade<CATEGORY, expression::unit>                                  \
-  , public expression::unit                                                    \
-{                                                                              \
-  template <typename... Ts>                                                    \
-  constexpr CATEGORY(Ts&&... xs)                                               \
-    : expression::unit {std::forward<Ts>(xs)...}                               \
-  {}                                                                           \
-                                                                               \
-  template <typename... Ts>                                                    \
-  decltype(auto) operator()(Ts&&...) const noexcept                            \
-  {                                                                            \
-    std::cerr << ";\n"                                                         \
-              << "; semantics\t; category " << #CATEGORY << std::endl;         \
-    std::cerr << ";\t\t; invoked default value!\n"                             \
-              << ";\t\t; two situations are possible;\n"                       \
-              << ";\t\t;   (1) the message has not been accepted (a.k.a meessage == nullptr)\n" \
-              << ";\t\t;   (2) called explicitly (in many cases, meaning no action is required)" << std::endl; \
-    return DEFAULT_VECTOR;                                                     \
-  }                                                                            \
-}
+#define PRINT_SEMANTICS_CATEGORY_MESSAGE(...)
 #endif // NDEBUG
 
+#define DEFINE_SEMANTICS_CATEGORY(CATEGORY, DEFAULT_VECTOR)                    \
+template <typename...>                                                         \
+struct CATEGORY;                                                               \
+template <>                                                                    \
+struct CATEGORY<expression::unit>                                              \
+  : public facade<CATEGORY, expression::unit>                                  \
+  , public expression::unit                                                    \
+{                                                                              \
+  template <typename... Ts>                                                    \
+  constexpr CATEGORY(Ts&&... xs)                                               \
+    : expression::unit {std::forward<Ts>(xs)...}                               \
+  {}                                                                           \
+                                                                               \
+  template <typename... Ts>                                                    \
+  decltype(auto) operator()(Ts&&...) const noexcept                            \
+  {                                                                            \
+    PRINT_SEMANTICS_CATEGORY_INFORMATION(CATEGORY)                             \
+    return DEFAULT_VECTOR;                                                     \
+  }                                                                            \
+}
 
 #ifndef NDEBUG
-#define DEFINE_SEMANTICS_CATEGORY_SPECIALIZATION(CATEGORY, TYPE, ...)          \
-template <>                                                                    \
-struct CATEGORY<TYPE>                                                          \
-  : public facade<CATEGORY, TYPE>                                              \
-{                                                                              \
-  vector_type operator()(const TYPE::ConstPtr& message) const                  \
-  {                                                                            \
-    if (message) __VA_ARGS__ else return CATEGORY<expression::unit> {}();      \
-  }                                                                            \
-};
+#define PRINT_SEMANTICS_CATEGORY_SPECIALIZAION_INFORMATION(CATEGORY, TYPE)     \
+std::cerr << ";\n"                                                             \
+          << "; semantics\t; category " << #CATEGORY << std::endl;             \
+std::cerr << ";\t\t; specialized for " << #TYPE << std::endl;
 #else // NDEBUG
+#define PRINT_SEMANTICS_CATEGORY_SPECIALIZAION_INFORMATION(...)
+#endif // NDEBUG
+
 #define DEFINE_SEMANTICS_CATEGORY_SPECIALIZATION(CATEGORY, TYPE, ...)          \
 template <>                                                                    \
 struct CATEGORY<TYPE>                                                          \
@@ -74,13 +56,10 @@ struct CATEGORY<TYPE>                                                          \
 {                                                                              \
   vector_type operator()(const TYPE::ConstPtr& message) const                  \
   {                                                                            \
-    std::cerr << ";\n"                                                         \
-              << "; semantics\t; category " << #CATEGORY << std::endl;         \
-    std::cerr << ";\t\t; specialized for " << #TYPE << std::endl;              \
+    PRINT_SEMANTICS_CATEGORY_SPECIALIZAION_INFORMATION(CATEGORY, TYPE)         \
     if (message) __VA_ARGS__ else return CATEGORY<expression::unit> {}();      \
   }                                                                            \
 };
-#endif // NDEBUG
 
 
 #endif // INCLUDED_BEHAVIORS_SEMANTICS_DEFINE_HPP
